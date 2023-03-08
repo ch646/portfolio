1d685ac257343399d1b5473757c11a3554753c4b
Macharia Christabel <christabelwambui678@gmail.com>
2023-03-08 20:50:52 +0300

    commit changes

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'title': 'Homepage'})
 
 def cv(request):
     return render(request, 'cv.html')
