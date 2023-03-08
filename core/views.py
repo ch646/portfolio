a5ca9a9da9cf274e679a5c2adab943c8d318a594
Macharia Christabel <christabelwambui678@gmail.com>
2023-03-08 15:41:21 +0300

    commit changes

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def cv(request):
+    return render(request, 'cv.html')
+
+def hireme(request):
+    return render(request,'hire-me.html')
+
+def projects(request):
+    return render(request, 'projects.html')
